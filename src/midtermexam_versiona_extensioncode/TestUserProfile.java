02b10f3995ede70c5d37202996eb4a6e7c69caa4
dhilshad123 <dilshadyouseph944@gmail.com>
2022-02-22 10:40:42 -0500

    adding mainfile to remote repo

--- src/midtermexam_versiona_extensioncode/TestUserProfile.java
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class TestUserProfile {
     public static void main(String[] args)
     {
+         Scanner sc=new Scanner(System.in);
        String[] genres = {"Comedy", "Drama", "Action", "Mystery"};
        System.out.println("These are the Available genres: ");
        for(int i=0;i<genres.length;i++)
        {
            System.out.println("    "+genres[i]);
        }
-        Scanner sc=new Scanner(System.in);
+       
         System.out.print("Enter your Name: ");
         String name=sc.nextLine();
        
-        Scanner obj1=new Scanner(System.in);
+        Scanner avilableGenree=new Scanner(System.in);
         System.out.print("Genre: ");
         String genre=sc.nextLine();
      
