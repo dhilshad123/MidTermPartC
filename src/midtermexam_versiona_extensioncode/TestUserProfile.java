afe83dda2511947577e3eba5a83f54a93efcdaa8
dhilshad123 <dilshadyouseph944@gmail.com>
2022-02-22 10:22:48 -0500

    adding mainfile to remote repo

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,27 @@
+package midtermexam_versiona_extensioncode;
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dhils
+ */
+public class TestUserProfile {
+        private String name;
+        
+        Scanner sc = new Scanner(System.in);
+        public static void main(String[] args){
+            
+        
+        System.out.print("enter your name");
+        name = sc.nextLine();
+        
+        UserProfile[] users = new UserProfile();
+        
+    
+    
+}
